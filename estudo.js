fbb24c7e173e07f4561a02f96b2f516bfa168174
rhuancavalcanti <110117224+rhuancavalcanti@users.noreply.github.com>
2022-10-05 12:48:06 -0300

    aula 05/10

--- estudo.js
+++ estudo.js
@@ -1,6 +1,7 @@
 let tabuada = 111;
 function escreva(){
     document.write("Tabuada do " + tabuada + "<br>");
+    
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
     document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
@@ -13,3 +14,22 @@ function escreva(){
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
 
+let lista = ["professor" , "professor" , "professor" , "professor"]
+
+function mostraLista(){
+    document.write("Tamanho da Lista" + lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write("professor:" + lista[i] + "<br>")
+
+    }
+  }
+  function multiplica(){
+     for(let i = 1; i <= 10; i++){
+        document.write("O valor do i = " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+        document.write(i + " x "+j+" = " + (i*j) + "<br>");
+        }
+        document.write("<br>");
+     }
+
+  }
