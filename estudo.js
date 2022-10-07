4d009467377d4eae2503f93cbf2e0a7e8de0d283
rhuancavalcanti <110117224+rhuancavalcanti@users.noreply.github.com>
2022-10-07 09:08:56 -0300

    aula 07/10

--- estudo.js
+++ estudo.js
@@ -33,3 +33,14 @@ function mostraLista(){
      }
 
   }
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    for(let i = 1;  i <= t; i++){
+    let r = v * (1+(j/100));
+    v = r; 
+    }
+    document.write("Resultado" + r);
+
+}
